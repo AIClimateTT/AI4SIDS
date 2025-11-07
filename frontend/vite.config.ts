71a610782f28dfcfe13573944daf58c99568986d
Devon Murray <murraydevon14@gmail.com>
2025-11-06 21:34:42 -0400

    minor fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,9 @@ const config = defineConfig({
     }),
     tailwindcss(),
     netlify(), // ← add this
-    tanstackStart(),
+    tanstackStart({
+      customViteReactPlugin: true,
+    }),
     viteReact(),
   ],
 })
